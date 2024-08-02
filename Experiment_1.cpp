40b43c6d6c6ebcbf290f2a53ae0f253da08abcab
BoraxParth <parthborikar05@gmail.com>
2024-08-02 09:30:41 +0530

    Experiment_1.cpp

--- Experiment_1.cpp
+++ Experiment_1.cpp
@@ -1,13 +1,62 @@
-#include<iostream>
+// Parth Borikar
+// 23070123096
+// Entc B1
+//Experiment 1-Downloading and Installing VS Code, (Hello World and Calculator program)
+
+#include <iostream>
 using namespace std;
 int main()
 {
-    int sum=0,n,i;
-    cout<<"Enter the number of natural numbers you want to add : ";
-    cin>>n;
-    for(i=0;i<=n;i++)
-    {
-        sum= sum+i;
-    }
-    cout<<"The sum of first "<<n<<" is "<<sum;
+string a;
+
+    
+    cout<<"Enter your name  ";
+    getline(cin,a);
+    cout<<"Hello "<<a;
+    return 0;
+} 
+
+
+// ADDITION
+#include <iostream>
+using namespace std;
+int main()
+{ int a,b,c;
+cout << "enter first number ";
+cin >> a;
+cout << "enter the second number ";
+cin >> b;
+c=a+b;
+cout << "the sum is "<<c;
+return 0;
+} 
+
+
+// DIVISION
+#include <iostream>
+using namespace std;
+int main()
+{ float a,b,c;
+cout << "enter first number ";
+cin >> a;
+cout << "enter the second number ";
+cin >> b;
+c=a/b;
+cout << "the QUOTIENT is "<<c;
+return 0;
 }
+
+// MULTIPLICATION
+
+#include <iostream>
+using namespace std;
+int main()
+{ float a,b,c;
+cout << "enter first number ";
+cin >> a;
+cout << "enter the second number ";
+cin >> b;
+c=a*b;
+cout << "the RESULT is "<<c;
+return 0;
+} 
