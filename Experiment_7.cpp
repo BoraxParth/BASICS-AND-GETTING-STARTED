acdb2b21deec88f9aa13fbfae889d535d642cc0d
BoraxParth <parthborikar05@gmail.com>
2024-08-09 09:17:26 +0530

    Arrays and .cpp

--- Experiment_7.cpp
+++ Experiment_7.cpp
@@ -27,3 +27,23 @@ int main()
 }
 
 //7.2 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int s,i,j;
+    cout<<"Enter the number of elements you want to enter : ";
+    cin>>s;
+    int a[s];
+    for(i=0;i<=s-1;i++)
+    {
+        cout<<"Enter element no "<<i+1<<" ";
+        cin>>a[i];
+    }
+    cout<<"The reversed elements of the array are: ";
+    for(j=s-1;j>=0;j--)
+    {
+    cout<<" "<<a[j];
+    }
+    return 0;
+}
