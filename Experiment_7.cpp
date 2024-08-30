3bd79ec7ac19fc852fd9aa868c2a3b12ecfe5a4a
BoraxParth <parthborikar05@gmail.com>
2024-08-30 10:42:42 +0530

    Experiment_7.cpp

--- Experiment_7.cpp
+++ Experiment_7.cpp
@@ -47,3 +47,155 @@ int main()
     }
     return 0;
 }
+
+//7.3 Sum & average of array elements
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n, i, j;
+     float avg, s = 0;
+cout << "Enter the number of elements: ";
+cin >> n;
+int a[n];
+cout << "Enter array elements: ";
+for( i = 0; i<n ; i++)
+{
+    cin >> a[i];
+}
+
+for (j= 0 ; j<n; j++)
+{
+    s = a[j]+s;
+}
+avg = s/n;
+cout << "The sum of elements of the givne array is: "<<s<<endl;
+cout << "The average of the given array is: "<< avg<<endl;
+
+return 0;
+}
+
+//7.4 Maximum and minimum
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n, i, j, max, min;
+cout << "Enter the number of elements: ";
+cin >> n;
+int a[n];
+cout << "Enter array elements: ";
+for( i = 0; i<n ; i++)
+{
+    cin >> a[i];
+}
+max = a[0];
+min = a[0];
+for (j= 1 ; j<n; j++)
+{
+ if (max<a[j])
+
+ {
+    max = a [j];
+ }
+ if (min > a[j])
+ {
+    min = a[j];
+ }
+}
+cout <<"The maximum element in the given array is: "<<max<<endl;
+cout << " The minimun element in the array is: "<<min<<endl;
+return 0;
+
+}
+
+//7.5 Search element
+#include<iostream>
+using namespace std;
+int main()
+{
+     int n, i, j, s, c = 0, flag = 0;
+cout << "Enter the number of elements: ";
+cin >> n;
+int a[n];
+cout << "Enter array elements: ";
+for( i = 0; i<n ; i++)
+{
+    cin >> a[i];
+}
+cout << "Enter an element to be searched in an array: ";
+    cin >> s;
+for (j= 0 ; j<n; j++)
+{
+    if ( a[j]==s)
+    {
+cout<< "The element"<<" "<< s<< " " << "is present at location: "<<j<<endl;
+c++;
+flag =1;
+    }
+}
+
+if( flag ==0)
+{
+    cout<< "The element"<< " "<< s << " "<< "is not present in the given array";
+}
+else
+{
+    cout << "The element" << " "<< s << " "<< "occurs"<< " "<< c << " "<< "times.";
+}
+return 0;
+}
+
+
+// USER INPUT
+#include <iostream>
+using namespace std;
+int main()
+{
+    char s[]= "Parth";
+    cout<<s<<endl;
+    return 0;
+}
+
+//7.7 CONCATENATION
+#include<iostream>
+using namespace std;
+int main() 
+{
+    string name("Parth");
+    string surname("Borikar");
+    name.append(surname);
+    cout<<name<<endl;
+}
+
+//7.7 REVERSE STRING
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+string a= "Prakhar";
+reverse(a.begin(), a.end());
+cout<<"reverse string is:"<<a<<endl;
+return 0;
+}
+
+//7.8 PALINDROME
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main() 
+{
+    string s1, s2;
+    cout << "Enter a word to check";
+    cin>>s1;
+    s2=s1;
+    reverse(s1.begin(), s1.end());
+    if (s2==s1) 
+    {
+        cout<<"Yes! It is a palindrome";
+    }
+    else cout<<"No! It is not a palindrome";
+}
