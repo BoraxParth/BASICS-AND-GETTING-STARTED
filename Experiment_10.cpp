f1f61c9b8dedac085859e37c063c36876c94a965
BoraxParth <parthborikar05@gmail.com>
2024-09-06 10:04:50 +0530

    Experiment_10.cpp

---
+++ Experiment_10.cpp
@@ -0,0 +1,53 @@
+//Name - Parth Borikar
+//PRN -  23070123096
+//Batch -  ENTC B1
+
+//Program 1 (Swap numbers using call by value)
+#include<iostream>
+using namespace std;
+void swap (int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int a = 7, b = 9;
+    cout<<"Before swap: "<<a<<" "<<b<<endl;
+    swap(a, b);
+    cout<<"After swap: "<<a<<" "<<b<<endl;
+}
+
+//Output
+/*
+Before swap: 7 9
+After swap: 7 9
+/*
+
+//Program 2 (Swap number using call by reference)
+#include<iostream>
+using namespace std;
+void swap (int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a = 7, b = 9;
+    cout<<"Before swap: "<<a<<" "<<b<<endl;
+    swap(&a, &b);
+    cout<<"After swap: "<<a<<" "<<b<<endl;
+}
+
+//Output
+/*
+Before swap: 7 9
+After swap: 9 7
+*/
